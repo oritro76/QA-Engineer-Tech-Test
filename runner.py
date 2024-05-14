a322337a81b1b13e89d778448f33edb274fe7a2c
oritro76 <aritra.banerjee207@gmail.com>
2024-05-14 06:37:03 +0200

    update runner.py

--- runner.py
+++ runner.py
@@ -10,14 +10,11 @@ from settings import BASE_URL
 arguments = []
 
 if os.environ.get("MOCK_SERVER") == "0":
-    # Arguments to pass to pytest
     arguments = ["-n", "auto"]
 
 test_env = os.environ.copy()
-# Execute pytest with the arguments
 exit_code = subprocess.call(["pytest"] + arguments, env=test_env)
 
-# Handle the exit code (optional)
 if exit_code == 0:
   print("Pytest execution successful!")
 else:
